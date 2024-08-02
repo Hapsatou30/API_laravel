83c6e72cdc39c1122bb617b914a2c0a6b2443806
Haps <thiamhapstou@gmail.com>
2024-08-02 18:37:09 +0000

    ajout des methodes et des routes pour l'auth

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ArticleController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Déclare une ressource API pour le contrôleur des articles
 Route::apiResource('articles', ArticleController::class);
+
+// Route pour l'inscription d'un utilisateur
+Route::post('/register', [AuthController::class, 'register']);
+
+// Route pour la connexion d'un utilisateur
+Route::post('/login', [AuthController::class, 'login']);
+
+// Route pour la déconnexion d'un utilisateur, protégée par le middleware 'auth:sanctum'
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+// Route pour obtenir les informations de l'utilisateur authentifié, protégée par le middleware 'auth:sanctum'
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    // Retourne les informations de l'utilisateur actuellement authentifié
+    return $request->user();
+});
